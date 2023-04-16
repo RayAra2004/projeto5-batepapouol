0228e88d5c6e8fa016e7d2fda8b76fad08123806
RayAra2004 <raynan.araujo@gmail.com>
2023-04-16 12:37:49 -0300

    Tela de Entrada

--- script/script.js
+++ script/script.js
@@ -14,12 +14,20 @@ function rederizaMesangens(msgs){
     const mensagens = msgs.data;
     const ul = document.querySelector('ul');
     ul.innerHTML = '';
+    /*
     mensagens.forEach(mensagem => {
         ul.innerHTML += `
         <li class="msg_all" data-test="message">   
             <p class="hora">${mensagem.time}</p><p class="msg"> 
             <span class="user">${mensagem.from}</span> para <span class="user">${mensagem.to}:
             </span> ${mensagem.text}</p>
+        </li>
+        `
+    });*/
+    mensagens.forEach(mensagem => {
+        ul.innerHTML += `
+        <li class="msg_all" data-test="message">   
+            <p class="hora">(${mensagem.time})</p><p class="msg"> <span class="user">${mensagem.from}</span> para <span class="user">${mensagem.to}:</span>  ${mensagem.text}</p>
         </li>
         `
     });
@@ -29,19 +37,31 @@ function userAtivo(){
     axios.post('https://mock-api.driven.com.br/api/vm/uol/status', {name: nome});
 }
 
+
 function entrarSala(){
-    nome = prompt("Informe seu nome:");
+    nome = document.querySelector(".tela_entrada input").value;
 
     const promisse = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', {name: nome});
     promisse.then(function(){
+        const tela_entrada = document.querySelector('.tela_entrada');
+        tela_entrada.style.display = 'none';
+        capturaMensagens();
+        setTimeout(function(){
+            const tela_loading = document.querySelector('.tela_loading');
+            tela_loading.style.display = 'none';
+        }, 1000);
         setInterval(capturaMensagens, 3000);
         setInterval(userAtivo, 5000);
     });
-    promisse.catch(entrarSala);
+    promisse.catch(function(){
+        alert("Nome já em uso");
+        document.querySelector(".tela_entrada input").value = '';
+    });
 }
 
 function enviarMensagem(){
-    const textoDigitado = document.querySelector('input').value;
+    const footer = document.querySelector('.footer');
+    const textoDigitado = footer.querySelector('input').value;
     const request =
         {
             from: nome,
@@ -52,10 +72,19 @@ function enviarMensagem(){
 
     const promisse = axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", request);
     promisse.then(function(){
-        document.querySelector('input').value = '';
+        footer.querySelector('input').value = '';
+        capturaMensagens();
     })
 
-    promisse.catch(res => window.location.reload());
+    promisse.catch(function() {
+        window.location.reload()
+    });
 }
 
-entrarSala();
+
+function teste(event){
+    var x = event.keyCode; // Obtém o valor Unicode (decimal)
+    if(x === 13){
+        enviarMensagem();
+    }
+}
